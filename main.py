76056e837f28fe973f214759f5a524f1ba88179e
QuasiXD <abhirajc08@gmail.com>
2026-02-21 17:42:55 +0530

    Finished UI

--- main.py
+++ main.py
@@ -35,6 +35,39 @@ class WeatherApp(QWidget):
         self.emoji_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.description_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
+        self.city_label.setObjectName("city_label")
+        self.city_input.setObjectName("city_input")
+        self.get_weather_button.setObjectName("get_weather_button")
+        self.temperature_label.setObjectName("temperature_label")
+        self.emoji_label.setObjectName("emoji_label")
+        self.description_label.setObjectName("description_label")
+
+        self.setStyleSheet("""
+            QLabel,QPushButton{
+                           font-family: calibri;
+                           }
+            QLabel#city_label{
+                           font-size: 40px;
+                           font-style: italic;
+                           }
+            QLineEdit#city_input{
+                           font-size: 40px;
+                           }
+            QPushButton#get_weather_button{
+                           font-size: 30px;
+                           font-weight: bold;
+                           }
+            QLabel#temperature_label{
+                            font-size: 75px;
+                            }
+            QLabel#emoji_label{
+                            font-size: 100px;
+                            }
+            QLabel#description_label{
+                            font-size: 50px;
+                            }
+                           """)
+
 
 
 

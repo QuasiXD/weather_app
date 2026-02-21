99599af3914d8990e8c61c3e2f5d01ee1bee6fd6
QuasiXD <abhirajc08@gmail.com>
2026-02-21 17:31:14 +0530

    Static Version

--- main.py
+++ main.py
@@ -6,8 +6,37 @@ from PyQt6.QtCore import Qt
 class WeatherApp(QWidget):
     def __init__(self):
         super().__init__()
+        self.city_label = QLabel("Enter City Name: ",self)
+        self.city_input = QLineEdit(self)
+        self.get_weather_button = QPushButton("Get Weather",self)
+        self.temperature_label = QLabel("21℃",self)
+        self.emoji_label = QLabel("☀️",self)
+        self.description_label = QLabel("Sunny",self)
+        self.initUI()
 
-    
+    def initUI(self):
+        self.setWindowTitle("Weather App")
+
+        vbox = QVBoxLayout()
+
+        vbox.addWidget(self.city_label)
+        vbox.addWidget(self.city_input)
+        vbox.addWidget(self.get_weather_button)
+        vbox.addWidget(self.temperature_label)
+        vbox.addWidget(self.emoji_label)
+        vbox.addWidget(self.description_label)
+
+        self.setLayout(vbox)
+
+        self.city_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.city_input.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.temperature_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.emoji_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.description_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
